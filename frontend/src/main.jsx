f2f78c038cb1f3e44d9838c6e3e1ddd7bb015f26
Alatriz18 <68755603+Alatriz18@users.noreply.github.com>
2025-12-12 08:16:19 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+console.log("URL ACTUAL:", window.location.href);
+ alert("¡Alto! Mira la barra de direcciones ahora mismo. ¿Ves el token?");
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
